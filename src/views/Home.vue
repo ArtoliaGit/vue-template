70f0df63c954133ac97fcd440d26ba8e226e2476
artolia <dxf1994@gmail.com>
2020-04-07 17:11:12 +0800

    增加验证码插件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
-    Hello Vue
+    <verify
+      @success="alert('success')"
+      @error="alert('error')"
+      :type="4"
+    />
   </div>
 </template>
+
+<script>
+import Verify from 'vue2-verify';
+
+export default {
+  name: 'Home',
+  components: {
+    Verify,
+  },
+  methods: {
+    alert(text) {
+      console.log(text);
+    },
+  },
+};
+</script>
